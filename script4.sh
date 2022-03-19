8a5de600bf75e183fedfbfc9a4a2eee183201214
lepeshcoder <76098636+lepeshcoder@users.noreply.github.com>
2022-03-19 03:58:38 +0300

    Delete script4.sh

--- script4.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [[ $# -ne 2 ]]; 
-then echo -e "This command must include 2 parameters\n1)source file\n2)executable file" >&2
-elif ! [[ -f $1 ]]; 
-then echo "$1 is not a valid file" >&2
-else gcc $1 -o $2 && ./$2;
-fi
